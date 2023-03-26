907ef16c0f2b17d79055df6e8d55d850079bc96a
YusahO <ziraelka1251@gmail.com>
2023-03-26 22:01:04 +0300

    ryr

--- lab4/lab4_1.asm
+++ lab4/lab4_1.asm
@@ -1,61 +1,121 @@
-PUBLIC mat, mwidth, mheight
+PUBLIC mat, mcols, mrows
 
 STK SEGMENT PARA STACK 'STACK'
     DB 200h DUP (?)
 STK ENDS
 
-DATASEG SEGMENT PARA PUBLIC 'DATA'
-    mat DB 51h DUP (0) 
-    mwidth DB 0
-    mheight DB 0
+DSEG SEGMENT PARA PUBLIC 'DATA'
+    mat DB 51h DUP (?) 
+    matches DB 0
+    mcols DB 0
+    mrows DB 0
 
-    input_width_prompt DB "Input width: ", '$'
-    input_height_prompt DB "Input height: ", '$'
+    input_rows_prompt DB "Input rows amount (1-9): ", '$'
+    input_cols_prompt DB "Input cols amount (1-9): ", '$'
+    input_elem_prompt DB "Input element: ", '$'
+    cur_row_prompt DB "Row ", '$'
     newline DB 13, 10, '$'
-DATASEG ENDS
+DSEG ENDS
 
 CSEG SEGMENT PARA PUBLIC 'CODE'
-    ASSUME CS:CSEG, DS: DATASEG, SS:STK
+    ASSUME CS:CSEG, DS: DSEG, SS:STK
 
 print_newline:
     MOV DX, OFFSET newline
     MOV AH, 09h
     INT 21h
-    ret
+    RET
+
+input_matrix PROC
+    XOR BX, BX
+    XOR CX, CX
+    MOV CL, mrows
+input_loop1:
+    PUSH CX
+    MOV CL, mcols
+    XOR SI, SI
+    input_loop2:
+        MOV AH, 01h
+        INT 21h
+
+        MOV mat[BX][SI], AL
+
+        MOV DL, ' '
+        MOV AH, 02h
+        INT 21h
+
+        INC SI
+        LOOP input_loop2
+
+    CALL print_newline
+    ADD BL, mcols
+    POP CX
+    LOOP input_loop1
+
+    RET
+input_matrix ENDP
+
+print_matrix PROC
+    XOR BX, BX
+    XOR CX, CX
+    MOV CL, mrows
+print_loop1:
+    PUSH CX
+    MOV CL, mcols
+    MOV AH, 02h
+    XOR SI, SI
+    print_loop2:
+        MOV DL, mat[BX][SI]
+        INT 21h
+        
+        MOV DL, ' '
+        INT 21h
+        INC SI
+        LOOP print_loop2
+
+    CALL print_newline 
+    ADD BL, mcols
+    POP CX
+    LOOP print_loop1
+
+    RET
+print_matrix ENDP
+
+delete_matching_columns PROC
+
+delete_matching_columns ENDP
 
 main:
-    MOV AX, DATASEG
+    MOV AX, DSEG
     MOV DS, AX
 
-    MOV DX, OFFSET input_width_prompt
+    MOV DX, OFFSET input_rows_prompt
     MOV AH, 09h
     INT 21h
 
     MOV AH, 01h
     INT 21h
 
-    MOV mwidth, AL
+    SUB AL, 30h
+    MOV mrows, AL
 
     CALL print_newline
 
-    MOV DX, OFFSET input_height_prompt
+    MOV DX, OFFSET input_cols_prompt
     MOV AH, 09h
     INT 21h
 
     MOV AH, 01h
     INT 21h
 
-    MOV mheight, AL
+    SUB AL, 30h
+    MOV mcols, AL
 
     CALL print_newline
 
-    MOV AH, 02h
-
-    MOV DL, mwidth
-    INT 21h
-
-    MOV DL, mheight
-    INT 21h
+    CALL input_matrix
+    CALL print_newline
+    CALL print_matrix
 
     MOV AH, 4Ch
     INT 21h

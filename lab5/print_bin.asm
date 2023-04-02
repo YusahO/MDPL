2d0ed975dc6638c6b28587dfb0f0866402a8ef97
YusahO <ziraelka1251@gmail.com>
2023-04-02 17:38:03 +0300

    change binary printing

--- lab5/print_bin.asm
+++ lab5/print_bin.asm
@@ -1,43 +1,24 @@
 public print_number_bin
 extrn number: word
 
-STK segment para stack 'STACK'
-    db 200h dup (?)
-STK ends
-
 CSEG segment para public 'CODE'
-    assume cs:CSEG, ss:STK
+    assume cs:CSEG
 
 print_number_bin proc near
     mov cx, 16
-    mov ax, number
-
-digits_bin_store_in_stack:
-    xor dx, dx
-    mov bx, 2
-    div bx
-    
-    add dl, '0'
-    push dx
-
-    cmp ax, 0
-    je print_digits_bin
-    loop digits_bin_store_in_stack
-
-print_digits_bin:
+    mov bx, number
     mov ah, 02h
-    mov ch, 16
-    sub ch, cl
-    mov cl, ch
-    inc cl
-    xor ch, ch
-    
+
 print_digits_bin_loop:
-    pop dx
+    mov dl, 30h
+    rol bx, 1
+    jnc print_zero_bin
+    inc dl
+
+print_zero_bin:
     int 21h
     loop print_digits_bin_loop
 
-    ret
 print_number_bin endp
 
 CSEG ends

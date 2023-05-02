1b367cf473e25df54e716cba82b2bdc261a9382d
YusahO <ziraelka1251@gmail.com>
2023-05-02 19:04:47 +0300

    ryr

--- lab8/src/main_2.cpp
+++ lab8/src/main_2.cpp
@@ -23,9 +23,9 @@ double sinAsmHalf() {
     double half = 2;
 
     __asm__ (
-        "fld %1\n"
         "fldpi\n"
-        "fdiv %%ST(0), %%ST(1)\n"
+        "fld %1\n"
+        "fdivp %%ST(1), %%ST(0)\n"
         "fsin\n"
         "fstp %0\n"
         : "=m"(res)

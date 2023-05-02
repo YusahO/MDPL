74212cbd430b059bf1b3124abd429fd29570298e
YusahO <ziraelka1251@gmail.com>
2023-05-03 02:36:21 +0300

    gyg

--- lab8/src/main_2.cpp
+++ lab8/src/main_2.cpp
@@ -36,7 +36,7 @@ double sinAsmHalf() {
 }
 
 void sinPi() {
-    std::cout << "\n_______PI______\n" << std::endl;
+    std::cout << "\n----- PI -----\n" << std::endl;
     std::cout << "sin(3.14): " << sin(3.14) << std::endl;
     std::cout << "sin(3.141596): " << sin(3.141596) << std::endl;
     std::cout << "sin(M_PI): " << sin(M_PI) << std::endl;
@@ -44,7 +44,7 @@ void sinPi() {
 }
 
 void sinPiHalf() {
-    std::cout << "\n_______PI / 2______\n" << std::endl;
+    std::cout << "\n----- PI / 2 -----\n" << std::endl;
     std::cout << "sin(3.14 / 2): " << sin(3.14 / 2) << std::endl;
     std::cout << "sin(3.141596): " << sin(3.141596 / 2) << std::endl;
     std::cout << "sin(M_PI / 2): " << sin(M_PI / 2) << std::endl;
